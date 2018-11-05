86e25bd854d4ac47faec0a6c6ae158c5cf4d0ab6
fatoufal <44146668+fatoufal@users.noreply.github.com>
2018-11-05 16:46:20 +0100

    Add files via upload

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,7 +1,29 @@
 function validation () {
-    var nomValue=document.querySelector("#nom").value ;
-    if(nomValue== ""){
+    var nomValue=document.querySelector("#nom").value.length ;
+    var nomValue=document.querySelector("#nom").value.length ;
+    var nomValue=document.querySelector("#nom").value.length ;
+    var nomValue=document.querySelector("#nom").value.length ;
+    var nomValue=document.querySelector("#nom").value.length ;
+    if(nomValue== 0){
         document.querySelector("#error").textContent="le nom est obligatoire";
-        document.querySelector("#nom + .errorItem").textContent="........";
+        document.querySelector("#nom + .errorItem").textContent=" ";
+
+    }
+    else if(nomValue<5){
+        document.querySelector("#error").textContent="le nombre de champs doit étre supérieur à 5";
+    }
+
+    else if(nomValue== 0){
+        document.querySelector("#error").textContent="le nom est obligatoire";
+        document.querySelector("#nom + .errorItem").textContent=" ";
+
+    }
+    else if(nomValue<5){
+        document.querySelector("#error").textContent="le nombre de champs doit étre supérieur à 5";
+    }
+
+    
+    else {
+        document.querySelector("#error").textContent="bienvenue   "  +  document.getElementById("nom").value;
     }
 }
